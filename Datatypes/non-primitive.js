b1c426fc856c99cffd74d0a54c540deecc6da035
Vikas Sutar <vikassutarfsd1020@gmail.com>
2025-11-26 21:40:18 +0530

    Add files via upload

---
+++ Datatypes/non-primitive.js
@@ -0,0 +1,24 @@
+// .......................Non-Primitive Data Types...........................
+
+// Object
+let person = {
+    name: "Vikas",
+    age: 22,
+    isStudent: true
+};
+
+console.log("Object:", person);
+
+
+// Array
+let fruits = ["Apple", "Banana", "Mango"];
+
+console.log("Array:", fruits);
+
+
+// Function
+function greet() {
+    return "Hello, JavaScript Developer!";
+}
+
+console.log("Function:", greet());
---
+++ Datatypes/premitive.js
@@ -0,0 +1,32 @@
+// .......................Primitive Data Types...........................
+
+// String
+let name = "Vikas";
+
+// Number
+let age =  22;
+
+// Boolean
+let isStudent = true;
+
+// Null
+let emptyValue = null;
+
+// Undefined
+let notAssigned;
+
+// Symbol
+let id = Symbol("unipueId");
+
+// BigInt
+let bignumber = 123456789012345678901234567890n;
+
+
+
+console.log("String:", name);
+console.log("Number:", age);
+console.log("Boolean:", isStudent);
+console.log("Null:", emptyValue);
+console.log("Undefined:", notAssigned);
+console.log("Symbol:", id);
+console.log("BigInt:", bignumber);
